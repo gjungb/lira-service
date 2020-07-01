3eea815d12157fac34ff7be164804fa77550e99a
Gerd Jungbluth <gerd.jungbluth@engawa.de>
2020-07-01 18:15:53 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lise-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'lira-service';
+}
